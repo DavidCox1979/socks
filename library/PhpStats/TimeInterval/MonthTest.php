f41e036a1cccfa02516661709a2fbce6576c901f
jshpro2 <jshpro2@9844b3f2-176b-11df-8596-193289140cd3>
2010-02-18 20:38:07 +0000

    describeEventTypes for Month Interval

--- library/PhpStats/TimeInterval/MonthTest.php
+++ library/PhpStats/TimeInterval/MonthTest.php
@@ -35,6 +35,30 @@ class PhpStats_TimeInterval_MonthTest extends PhpStats_TimeInterval_TestCase
         $this->assertEquals( 'January', $month->monthLabel() );
     }
     
+    function testDescribeEventTypes()
+    {
+        $this->logHour( 1, self::DAY, self::MONTH, self::YEAR, self::COUNT, array(), 'EventA' );
+        $this->logHour( 1, self::DAY, self::MONTH, self::YEAR, self::COUNT, array(), 'EventB' );
+        $month = new PhpStats_TimeInterval_Month( $this->getTimeParts() );
+        $this->assertEquals( array( 'EventA', 'EventB' ), $month->describeEventTypes(), 'returns array of distinct event types in use' );
+    }
+//    
+//    function testDescribeAttributeKeys()
+//    {
+//        $this->logThisDayWithHour( 1, array('a' => 1 ), 'eventA' );
+//        $this->logThisDayWithHour( 1, array('a' => 2 ), 'eventA' );
+//        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts() );
+//        $this->assertEquals( array('a'), $day->describeAttributeKeys(), 'returns array of distinct attribute keys in use' );
+//    }
+//    
+//    function testDescribeAttributeValues()
+//    {
+//        $this->logThisDayWithHour( 1, array('a' => 1 ), 'eventA' );
+//        $this->logThisDayWithHour( 1, array('a' => 2 ), 'eventA' );
+//        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts() );
+//        $this->assertEquals( array('a' => array( 1, 2 ) ), $day->describeAttributesValues(), 'returns array of distinct keys & values for attributes in use' );
+//    }
+    
     protected function getTimeParts()
     {
         return array(
