d4d192d55983752e3e304c222b9c840bf19fa497
jshpro2 <jshpro2@9844b3f2-176b-11df-8596-193289140cd3>
2010-03-22 02:59:12 +0000

    cleaned up tests

--- library/PhpStats/CompactorTest.php
+++ library/PhpStats/CompactorTest.php
@@ -9,12 +9,7 @@ class PhpStats_CompactorTest extends PhpStats_UnitTestCase
     
     function testLastCompacted()
     {
-        $timeParts = array(
-            'hour' => 1,
-            'day' => 1,
-            'month' => 1,
-            'year' => 2002
-        );
+        $timeParts = array('hour' => 1,'day' => 1,'month' => 1,'year' => 2002);
         
         $this->logHour( $timeParts );
         $hour = new PhpStats_TimeInterval_Hour( $timeParts );
@@ -26,22 +21,10 @@ class PhpStats_CompactorTest extends PhpStats_UnitTestCase
     
     function testLastCompacted2()
     {
-        $timeParts = array(
-            'hour' => 1,
-            'day' => 1,
-            'month' => 1,
-            'year' => 2002
-        );
-        
+        $timeParts = array('hour' => 1,'day' => 1,'month' => 1,'year' => 2002);
         $this->logHour( $timeParts );
         
-        $timeParts = array(
-            'hour' => 2,
-            'day' => 1,
-            'month' => 1,
-            'year' => 2002
-        );
-        
+        $timeParts = array('hour' => 2,'day' => 1,'month' => 1,'year' => 2002);
         $this->logHour( $timeParts );
         
         $hour = new PhpStats_TimeInterval_Hour( $timeParts );
