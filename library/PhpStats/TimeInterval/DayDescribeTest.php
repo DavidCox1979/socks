c61381a0a4aaec7bee3c22effd77e5e267e4113b
jshpro2 <jshpro2@9844b3f2-176b-11df-8596-193289140cd3>
2010-03-23 05:32:24 +0000

    pulled up test utility methods

--- library/PhpStats/TimeInterval/DayDescribeTest.php
+++ library/PhpStats/TimeInterval/DayDescribeTest.php
@@ -326,39 +326,5 @@ class PhpStats_TimeInterval_DayDescribeTest extends PhpStats_TimeInterval_DayTes
         $this->clearUncompactedEvents();
         $this->assertEquals( array('a'), $day->describeAttributeKeys(), 'excludes different years from describeAttributeKeys() (compacted)' );
 	}
-	 
-    protected function dayPlusOneTimeParts()
-    {
-		$day = $this->dayTimeParts();
-		$dayPlusOne = $day;
-        $dayPlusOne['day'] += 1;
-        return $dayPlusOne;
-    }
-    
-    protected function dayPlusOneMonthTimeParts()
-    {
-		$day = $this->dayTimeParts();
-		$dayPlusOneMonth = $day;
-        $dayPlusOneMonth['month'] += 1;
-        return $dayPlusOneMonth;
-    }
-    
-    protected function dayPlusOneYearTimeParts()
-    {
-		$day = $this->dayTimeParts();
-		$dayPlusOneYear = $day;
-        $dayPlusOneYear['year'] += 1;
-        return $dayPlusOneYear;
-    }
-    
-	protected function dayTimeParts()
-    {
-		$day = array(
-        	'hour' => 1,
-        	'day' => self::DAY,
-        	'month' => self::MONTH,
-        	'year' => self::YEAR
-        );
-        return $day;
-    }
+	
 }
