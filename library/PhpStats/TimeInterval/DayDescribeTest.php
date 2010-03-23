b70cc46052f6dde413aec2a8f66f6b8c60798c48
jshpro2 <DavidCox1979rocketmail.com>
2010-03-23 05:43:33 +0000

    split out test class

--- library/PhpStats/TimeInterval/DayDescribeTest.php
+++ library/PhpStats/TimeInterval/DayDescribeTest.php
@@ -4,7 +4,7 @@
 * with this package in the file LICENSE.txt.
 */
 class PhpStats_TimeInterval_DayDescribeTest extends PhpStats_TimeInterval_DayTestCase
-{    
+{
     function testDescribeEventTypes()
     {
         $this->logThisDayWithHour( 1, array(), 'eventA' );
@@ -44,61 +44,6 @@ class PhpStats_TimeInterval_DayDescribeTest extends PhpStats_TimeInterval_DayTes
         
         $day = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array(), false, false );
         $this->assertEquals( array(), $day->describeEventTypes(), 'when uncompacted hits are disabled, and day is not compacted, describeEventTypes should return empty array (even if an hour is compacted).' );
-    }
-    
-    function testDescribeAttributeKeys()
-    {
-        $this->logThisDayWithHour( 1, array('a' => 1 ), 'eventA' );
-        $this->logThisDayWithHour( 1, array('a' => 2 ), 'eventA' );
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts() );
-        $this->assertEquals( array('a'), $day->describeAttributeKeys(), 'returns array of distinct attribute keys in use' );
-    }
-
-    function testDescribeAttributeKeysCompacted()
-    {
-        $this->logThisDayWithHour( 1, array('a' => 1 ), 'eventA' );
-        $this->logThisDayWithHour( 1, array('a' => 2 ), 'eventA' );
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts() );
-        $day->compact();
-        $this->clearUncompactedEvents();
-        $this->assertEquals( array('a'), $day->describeAttributeKeys(), 'returns array of distinct attribute keys in use' );
-    }
-    
-    function testDescribeAttributeKeysUncompactedHitsDisabled() 
-    {
-		$this->logThisDayWithHour( 1, array('a' => 1 ), 'eventA' );
-        $this->logThisDayWithHour( 1, array('a' => 2 ), 'eventA' );
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array(), false, false );
-        $this->assertEquals( array(), $day->describeAttributeKeys(), 'when uncompacted hits are disabled, describeAttributeKeys should return empty array' );
-    }
-    
-    function testDescribeAttributeKeysEventType()
-    {
-        $this->logThisDayWithHour( 1, array('a' => 1 ), 'eventA' );
-        $this->logThisDayWithHour( 1, array('b' => 1 ), 'eventB' );
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts() );
-        $this->assertEquals( array('a'), $day->describeAttributeKeys('eventA'), 'returns array of distinct attribute keys in use (for specific event type)' );
-    }
-    
-    function testDescribeAttributeKeysEventType2()
-    {
-        $this->logThisDayWithHour( 1, array('a' => 1 ), 'eventA' );
-        $this->logThisDayWithHour( 1, array('b' => 1 ), 'eventB' );
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts() );
-        $this->assertEquals( array('b'), $day->describeAttributeKeys('eventB'), 'returns array of distinct attribute keys in use (for specific event type)' );
-    }
-    
-    function testDescribeAttributeKeysHoursCompacted()
-    {
-		$this->logThisDayWithHour( 1, array('a' => 1 ), 'eventA' );
-        $this->logThisDayWithHour( 1, array('a' => 2 ), 'eventA' );
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts() );
-        foreach( $day->getHours() as $hour )
-        {
-			$hour->compact();
-        }
-        $this->clearUncompactedEvents( true );
-        $this->assertEquals( array('a'), $day->describeAttributeKeys() );
     }
     
     function testDescribeAttributeValues()
@@ -214,14 +159,6 @@ class PhpStats_TimeInterval_DayDescribeTest extends PhpStats_TimeInterval_DayTes
         $this->assertEquals( array('a' => array( 1 ) ), $day->describeAttributesValues( 'typeA'), 'describing attribute values for specific event type should return values only for that type');
     }
     
-    function testDescribeAttributeKeysPresent()
-    {
-        $this->logHour( $this->now(), array('a' => 1 ), 'eventA', self::COUNT );
-        $this->logHour( $this->now(), array('a' => 2 ), 'eventA', self::COUNT );
-        $day = new PhpStats_TimeInterval_Day( $this->now() );
-        $this->assertEquals( array('a'), $day->describeAttributeKeys(), 'returns array of distinct keys attributes in use (when time interval is "now")' );
-    }
-    
     function testDescribeAttributeValuesPresent()
     {
         $this->logHour( $this->now(), array('a' => 1 ), 'eventA', self::COUNT );
@@ -258,73 +195,5 @@ class PhpStats_TimeInterval_DayDescribeTest extends PhpStats_TimeInterval_DayTes
     {
 		return $this->markTestIncomplete(); 
     }
-    
-    function testDescribeAttributeKeysExcludesDifferentDay()
-    {
-    	$this->logHour( $this->getTimeParts(), array( 'a' => 1 ) );
-        $this->logHour( $this->dayPlusOneTimeParts(), array( 'b' => 1 ) );
-        
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array(), false );
-        $this->assertEquals( array('a'), $day->describeAttributeKeys(), 'excludes different days from describeAttributeKeys()' );
-    }
-    
-    /** @todo also test for when hours are compacted? */
-    function testDescribeAttributeKeysExcludesDifferentDayCompacted()
-    {
-        $this->logHour( $this->getTimeParts(), array( 'a' => 1 ) );
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts() );
-        $day->compact();
-        
-        $this->logHour( $this->dayPlusOneTimeParts(), array( 'b' => 1 ) );
-        $day = new PhpStats_TimeInterval_Day( $this->dayPlusOneTimeParts() );
-        $day->compact();
-        
-        $this->clearUncompactedEvents();
-        $day = new PhpStats_TimeInterval_Day( $this->dayPlusOneTimeParts() );
-        $this->assertEquals( array('b'), $day->describeAttributeKeys(), 'excludes different days from describeAttributeKeys() (compacted)' );
-    }
-    
-    function testDescribeAttributeKeysExcludesDifferentMonth()
-    {
-        $this->logHour( $this->getTimeParts(), array( 'a' => 1 ) );
-        $this->logHour( $this->dayPlusOneMonthTimeParts(), array( 'b' => 1 ) );
-        
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array(), false );                                                  
-        $this->assertEquals( array('a'), $day->describeAttributeKeys(), 'excludes different months from describeAttributeKeys()' );
-    }
-    
-    /** @todo also test for when hours are compacted? */
-    function testDescribeAttributeKeysExcludesDifferentMonthCompacted()
-    {
-    	$this->logHour( $this->getTimeParts(), array( 'a' => 1 ) );
-        $this->logHour( $this->dayPlusOneMonthTimeParts(), array( 'b' => 1 ) );
-        
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array(), false ); 
-        
-        $day->compact();
-        $this->clearUncompactedEvents();
-        $this->assertEquals( array('a'), $day->describeAttributeKeys(), 'excludes different months from describeAttributeKeys() (compacted)' );
-    }
-    
-    function testDescribeAttributeKeysExcludesDifferentYear()
-    {
-        $this->logHour( $this->getTimeParts(), array( 'a' => 1 ) );
-        $this->logHour( $this->dayPlusOneYearTimeParts(), array( 'b' => 1 ) );
-        
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array(), false );
-        $this->assertEquals( array('a'), $day->describeAttributeKeys(), 'excludes different years from describeAttributeKeys()' );
-    }
-    
-    /** @todo also test for when hours are compacted? */
-    function testDescribeAttributeKeysExcludesDifferentYearCompacted()
-    {
-        $this->logHour( $this->getTimeParts(), array( 'a' => 1 ) );
-        $this->logHour( $this->dayPlusOneYearTimeParts(), array( 'b' => 1 ) );
-        
-        $day = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array(), false );
-        $day->compact();
-        $this->clearUncompactedEvents();
-        $this->assertEquals( array('a'), $day->describeAttributeKeys(), 'excludes different years from describeAttributeKeys() (compacted)' );
-	}
 	
 }
