070bdb4bd254d462c5715a703987175c112f60b6
jshpro2 <DavidCox1979rocketmail.com>
2010-05-25 02:02:16 +0000

    cleaned up tests

--- library/PhpStats/TimeInterval/DayKeysTest.php
+++ library/PhpStats/TimeInterval/DayKeysTest.php
@@ -3,7 +3,7 @@
 * This source file is subject to the new BSD license that is bundled
 * with this package in the file LICENSE.txt.
 */
-class PhpStats_TimeInterval_DayDescribeAttributeKeysTest extends PhpStats_TimeInterval_DayTestCase
+class PhpStats_TimeInterval_DayKeysTest extends PhpStats_TimeInterval_DayTestCase
 {
 	function testNoAttributesWhenUncompacted()
     {
