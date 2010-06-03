f713e2d8b6339c91381b49ae99619ddb781b27e7
jshpro2 <jshpro2@9844b3f2-176b-11df-8596-193289140cd3>
2010-06-03 19:44:13 +0000

    refactored, created filterByEventType method on select class

--- library/PhpStats/Select.php
+++ library/PhpStats/Select.php
@@ -5,22 +5,35 @@ class PhpStats_Select extends Zend_Db_Select
     {
         $this->filterByDay($timeParts);
         $this->where( '`hour` = ?', $timeParts['hour'] ) ;
+        return $this;
     }
     
     function filterByDay( $timeParts )
     {
         $this->filterByMonth($timeParts);
         $this->where( '`day` = ?', $timeParts['day'] ) ;
+        return $this;
     }
     
     function filterByMonth( $timeParts )
     {
         $this->filterByYear($timeParts);
         $this->where( '`month` = ?', $timeParts['month'] );
+        return $this;
     }
     
     function filterByYear( $timeParts )
     {
         $this->where( '`year` = ?', $timeParts['year'] );
+        return $this;
+    }
+    
+    function filterByEventType( $eventType )
+    {
+        if( !is_null( $eventType ) )
+        {
+            $this->where( 'event_type = ?', $eventType );
+        }
+        return $this;
     }
 }
