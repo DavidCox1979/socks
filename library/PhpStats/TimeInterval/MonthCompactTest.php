1617602ac450b325595ab2dbc4864b376791cd0d
jshpro2 <jshpro2@9844b3f2-176b-11df-8596-193289140cd3>
2010-06-14 19:55:55 +0000

    removed day EAV table

--- library/PhpStats/TimeInterval/MonthCompactTest.php
+++ library/PhpStats/TimeInterval/MonthCompactTest.php
@@ -220,7 +220,6 @@ class PhpStats_TimeInterval_MonthCompactTest extends PhpStats_TimeInterval_DayTe
 	protected function clearUncompactedEvents(  )
     {
 	    $this->db()->query('truncate table `socks_day_event`');
-	    $this->db()->query('truncate table `socks_day_event_attributes`');
 	    $this->db()->query('truncate table `socks_hour_event`');
         $this->db()->query('truncate table `socks_event`');
         $this->db()->query('truncate table `socks_event_attributes`');
