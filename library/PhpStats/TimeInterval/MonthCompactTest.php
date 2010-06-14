d51ffbbe8404a083212f0e23e03fa29e923560cb
jshpro2 <DavidCox1979rocketmail.com>
2010-06-14 20:37:07 +0000

    cleaned up DayCompact tests; hardened MonthCompact tests

--- library/PhpStats/TimeInterval/MonthCompactTest.php
+++ library/PhpStats/TimeInterval/MonthCompactTest.php
@@ -5,7 +5,7 @@
 */
 class PhpStats_TimeInterval_MonthCompactTest extends PhpStats_TimeInterval_DayTestCase
 {    
-    function testCompactSpecicEventType()
+    function testShouldCompactSpecicEventType()
     {
         $this->logThisDayWithHour( 1, array(), 'eventtype' );
         $month = $this->getMonth();
@@ -13,10 +13,10 @@ class PhpStats_TimeInterval_MonthCompactTest extends PhpStats_TimeInterval_DayTe
         $this->clearUncompactedEvents();
         
         $month = $this->getMonth();
-        $this->assertEquals( self::COUNT, $month->getCompactedCount('eventtype'), 'gets compacted count for specific event type' );
+        $this->assertEquals( self::COUNT, $month->getCompactedCount('eventtype'), 'should get compacted count for specific event type' );
     }
     
-    function testCompactAllEventTypes()
+    function testShouldCompactAllEventTypes()
     {
         $this->logThisDayWithHour( 1, array(), 'eventtype1' );
         $this->logThisDayWithHour( 1, array(), 'eventtype2' );
@@ -25,7 +25,78 @@ class PhpStats_TimeInterval_MonthCompactTest extends PhpStats_TimeInterval_DayTe
         $this->clearUncompactedEvents();
         
         $month = $this->getMonth();
-        $this->assertEquals( self::COUNT*2, $month->getCompactedCount(), 'gets compacted count for all event types' );
+        $this->assertEquals( self::COUNT*2, $month->getCompactedCount(), 'should get compacted count for all event types' );
+    }
+    
+    /**
+    * @expectedException Exception
+    */
+    function testWhenUncomapctedHitsDisabledCannotCompact()
+    {
+        $month = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array(), false, false );
+        $this->assertFalse( $month->canCompact(), 'when uncompacted queries are disabled, should not compact' );
+        $month->compact();
+    }
+    
+    /**
+    * @expectedException Exception
+    */
+    function testShouldNotCompactWhenFilteringWithAttributes()
+    {
+         $month = new PhpStats_TimeInterval_Month( $this->getTimeParts(), array( 'a' => 1 ));
+         $month->compact();
+    }
+    
+    function testAttributesThruConstructor()
+    {
+        $this->logThisDayWithHour( 1, array( 'a' => 1 ) );
+        $this->logThisDayWithHour( 1, array( 'a' => 2 ) );
+        
+        $month = $this->getMonth();
+        $month->compact();
+        $this->clearUncompactedEvents();
+        
+        $month = new PhpStats_TimeInterval_Month( $this->getTimeParts(), array( 'a' => 1 ) );
+        $this->assertEquals( 2, $month->getCount('click'), 'getCompactedCount should return count only for the requested attribute (passed to constructor)' );
+    }
+    
+    function testAttributesThruMethod()
+    {
+        $this->logThisDayWithHour( 1, array( 'a' => 1 ) );
+        $this->logThisDayWithHour( 1, array( 'a' => 2 ) );
+        
+        $month = $this->getMonth();
+        $month->compact();
+        $this->clearUncompactedEvents();
+        
+        $month = new PhpStats_TimeInterval_Month( $this->getTimeParts() );
+        $this->assertEquals( 4, $month->getCount('click', array( 'a' => 1 ) ), 'getCompactedCount should return count only for the requested attribute (passed to method)' );
+    }
+    
+    function testAttributesNone()
+    {
+        $this->logHour( $this->getTimeParts(), array( 'a' => 1 ) );
+        $this->logHour( $this->getTimeParts(), array( 'a' => 2 ) );
+        
+        $month = new PhpStats_TimeInterval_Month( $this->getTimeParts() );
+        $month->compact();
+        $this->clearUncompactedEvents();
+        
+        $month = new PhpStats_TimeInterval_Month( $this->getTimeParts(), array( 'a' => 3 ) );
+        $this->assertEquals( 0, $month->getCount('click'), 'when filtering on non-existant value, count should always equal 0' );
+    }
+    
+    function testNullMeansAll()
+    {
+        $this->logHour( $this->getTimeParts(), array( 'a' => 1 ) );
+        $this->logHour( $this->getTimeParts(), array( 'a' => 2 ) );
+        
+        $month = $this->getMonth();
+        $month->compact();
+        $this->clearUncompactedEvents();
+        
+        $month = new PhpStats_TimeInterval_Month( $this->getTimeParts(), array( 'a' => null ) );
+        $this->assertEquals( 2, $month->getCount('click'), 'passing null for an attribute is the same as not passing it' );
     }
     
     function testCompact2()
@@ -70,81 +141,124 @@ class PhpStats_TimeInterval_MonthCompactTest extends PhpStats_TimeInterval_DayTe
         $this->assertTrue( $month->hasBeenCompacted() );
     }
     
+    function testHasBeenCompactedWithZeroHits()
+    {
+        $month = $this->getMonth();
+        $month->compact();
+        
+        $month = $this->getMonth();
+        $this->assertTrue( $month->hasBeenCompacted() );
+    }
+    
     function testHasBeenCompactedWithAttribs()
     {
-    	return $this->markTestIncomplete();
-//        $this->logThisDayWithHour( 1, array( 'a' => 1 ), 'eventtype' );
-//        $month = $this->getMonth();
-//        $month->compact();
-//        $this->assertTrue( $month->hasBeenCompacted() );
+    	$this->logThisDayWithHour( 1, array( 'a' => 1 ), 'eventtype' );
+        $month = $this->getMonth();
+        $month->compact();
+        
+        $month = $this->getMonth();
+        $this->assertTrue( $month->hasBeenCompacted() );
     }
-//    
+    
     function testHasNotBeenCompacted()
     {
-    	return $this->markTestIncomplete();
-//        $month = $this->getMonth();
-//        $this->assertFalse( $month->hasBeenCompacted() );
+        $month = $this->getMonth();
+        
+        $month = $this->getMonth();
+        $this->assertFalse( $month->hasBeenCompacted() );
     }
-//    
+    
     function testHasNotBeenCompacted2()
     {
-    	return $this->markTestIncomplete();
-//        $timeParts = $this->getTimeParts();
-//        $timeParts['hour'] = 1;
-//        $hour = new PhpStats_TimeInterval_Hour( $timeParts );
-//        $hour->compact();
-//        $month = new PhpStats_TimeInterval_Day( $timeParts );
-//        $this->assertFalse( $month->hasBeenCompacted() );
+    	$timeParts = $this->getTimeParts();
+        $timeParts['day'] = 1;
+        $day = new PhpStats_TimeInterval_Day( $timeParts );
+        $day->compact();
+        $month = new PhpStats_TimeInterval_Month( $timeParts );
+        $this->assertFalse( $month->hasBeenCompacted() );
     }
-//    
+    
     function testHasBeenCompacted2()
     {
-    	return $this->markTestIncomplete();
-//        $timeParts = $this->getTimeParts();
-//        $timeParts['hour'] = 1;
-//        $month = new PhpStats_TimeInterval_Day( $timeParts );
-//        $month->compact();
-//        
-//        $month = new PhpStats_TimeInterval_Day( $timeParts );
-//        $this->assertTrue( $month->hasBeenCompacted() );
+    	$timeParts = $this->getTimeParts();
+        $timeParts['day'] = 1;
+        $month = new PhpStats_TimeInterval_Month( $timeParts );
+        $month->compact();
+        
+        $month = new PhpStats_TimeInterval_Month( $timeParts );
+        $this->assertTrue( $month->hasBeenCompacted() );
     }
-//    
+    
     function testClearsPreviouslyCompacted()
     {
-    	return $this->markTestIncomplete();
-//        $this->logThisDayWithHour( 1, array(), 'eventtype' );
-//        $month = $this->getMonth();
-//        $month->compact();
-//        $this->assertEquals( self::COUNT, $month->getCompactedCount('eventtype') );
-//        $month->compact();
-//        $this->assertEquals( self::COUNT, $month->getCompactedCount('eventtype'), 'Compact() clears previously compacted' );
+    	$this->logThisDayWithHour( 1, array(), 'eventtype' );
+        $month = $this->getMonth();
+        $month->compact();
+        $this->assertEquals( self::COUNT, $month->getCompactedCount('eventtype') );
+        $month->compact();
+        
+        $month = $this->getMonth();
+        $this->assertEquals( self::COUNT, $month->getCompactedCount('eventtype'), 'Compact() clears previously compacted' );
     }
-//    
+    
     function testExcludesDifferentEventType()
     {
-    	return $this->markTestIncomplete();
-//        $this->logThisDayWithHour( 1, array(), 'differentType' );
-//        $month = $this->getMonth();
-//        $month->compact();
-//        $this->assertEquals( 0, $month->getCompactedCount('click'), 'getCount should not include hits of a different type in it\'s summation' );
+    	$this->logThisDayWithHour( 1, array(), 'differentType' );
+        $month = $this->getMonth();
+        $month->compact();
+        
+        $month = $this->getMonth();
+        $this->assertEquals( 0, $month->getCompactedCount('click'), 'getCount should not include hits of a different type in it\'s summation' );
     }
 
-    function testChildrenHours()
+    function testChildrenDays()
     {
-    	return $this->markTestIncomplete();
-//        $this->logHour( $this->getTimeParts() );
-//        $month = new PhpStats_TimeInterval_Day( $this->getTimeParts() );
-//        
-//        $hours = $month->getHours();
-//        $this->assertEquals( 1, $hours[1]->getCount('click') );
-//        
-//        $month->compact();
-//        
-//        $month = $this->getMonth();
-//        $hours = $month->getHours();
-//        $this->assertEquals( 1, $hours[1]->getCount('click'), 'compacting the day should cause it\'s hours to be first compacted' );
+    	$this->logHour( $this->getTimeParts() );
+        $month = new PhpStats_TimeInterval_Month( $this->getTimeParts() );
+        
+        $days = $month->getDays();
+        $this->assertEquals( 1, $days[1]->getCount('click') );
+        
+        $month->compact();
+        
+        $month = $this->getMonth();
+        $days = $month->getDays();
+        $this->assertEquals( 1, $days[1]->getCount('click'), 'compacting the month should cause it\'s days to be first compacted' );
     }    
-//    
+    
+    function testEventTypesAndAttribs()
+    {
+        $this->logThisDayWithHour( 1, array( 'a' => 1 ), 'eventA' );
+        $this->logThisDayWithHour( 1, array( 'b' => 2 ), 'eventB' );
+        
+        $month = $this->getMonth();
+        $month->compact();
+        $this->clearUncompactedEvents();
+        
+        $month = new PhpStats_TimeInterval_Month( $this->getTimeParts() );
+        $this->assertEquals( self::COUNT, $month->getCount('eventA'), 'month should compact event_types seperately when there are attributes' );
+    }
+    
+    function testAutomaticMode()
+    {
+        return $this->markTestIncomplete();
+    }
+    
+    function testNonUniquesProperly()
+    {
+        $oneOClock = array( 'hour'=>1, 'day'=>self::DAY, 'month'=>self::MONTH, 'year'=>self::YEAR );
+        $this->logHour( $oneOClock, array( 'a' => 1 ), 'click', self::COUNT, '127.0.0.1' );
+        
+        $twoOClock = array( 'hour'=>1, 'day'=>self::DAY, 'month'=>self::MONTH, 'year'=>self::YEAR );
+        $this->logHour( $twoOClock, array( 'a' => 2 ), 'click', self::COUNT, '127.0.0.2' );
+        
+        $month = new PhpStats_TimeInterval_Month( array( 'month'=>self::MONTH, 'year'=>self::YEAR ) );
+        $month->compact();
+        
+        $month = $this->getMonth();
+        $this->assertEquals( self::COUNT * 2, $month->getCount( 'click', array(), false ), 'counts non-unique hits after compaction' );
+    }
+    
     function testAttributes()
     {
     	return $this->markTestIncomplete();
@@ -157,62 +271,9 @@ class PhpStats_TimeInterval_MonthCompactTest extends PhpStats_TimeInterval_DayTe
 //        $month = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array( 'a' => 1 ) );
 //        
 //        $this->assertEquals( self::COUNT, $month->getCount('click'), 'getCompactedCount should return count only for the requested attribute' );
-    } 
-//    
-    function testEventTypesAndAttribs()
-    {
-    	return $this->markTestIncomplete();
-//        $this->logThisDayWithHour( 1, array( 'a' => 1 ), 'eventA' );
-//        $this->logThisDayWithHour( 1, array( 'b' => 2 ), 'eventB' );
-//        
-//        $month = $this->getMonth();
-//        $month->compact();
-//        $this->clearUncompactedEvents();
-//        $month = new PhpStats_TimeInterval_Day( $this->getTimeParts() );
-//        
-//        $this->assertEquals( self::COUNT, $month->getCount('eventA'), 'day should compact event_types seperately when there are attributes' );
-    } 
+    }  
     
-    function testAutomaticMode()
-    {
-    	return $this->markTestIncomplete();
-//        $this->logThisDayWithHour( 1, array( 'a' => 1 ) );
-//        $this->logThisDayWithHour( 1, array( 'a' => 2 ) );
-//        
-//        $month = $this->getMonth();
-//        $month->getCount('click');
-//        $this->clearUncompactedEvents();
-//        $month = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array( 'a' => 1 ) );
-//        $this->assertEquals( self::COUNT, $month->getCount('click'), 'should compact automatically' );
-    }
-    
-    function testNonUniquesProperly()
-    {
-    	return $this->markTestIncomplete();
-//    	$oneOClock = array( 'hour'=>1, 'day'=>self::DAY, 'month'=>self::MONTH, 'year'=>self::YEAR );
-//        $this->logHour( $oneOClock, array( 'a' => 1 ), 'click', self::COUNT, '127.0.0.1' );
-//        
-//        $twoOClock = array( 'hour'=>1, 'day'=>self::DAY, 'month'=>self::MONTH, 'year'=>self::YEAR );
-//        $this->logHour( $twoOClock, array( 'a' => 2 ), 'click', self::COUNT, '127.0.0.2' );
-//        
-//        $wholeDay = array( 'day'=>self::DAY, 'month'=>self::MONTH, 'year'=>self::YEAR );
-//        $month = new PhpStats_TimeInterval_Day( $wholeDay );
-//        $month->compact();
-//        
-//        $this->assertEquals( self::COUNT * 2, $month->getCount( 'click', array(), false ), 'counts non-unique hits after compaction' );
-    }
-    
-    /**
-    * @expectedException Exception
-    */
-    function testWhenUncomapctedHitsDisabledCannotCompact()
-    {
-    	return $this->markTestIncomplete();
-//		$month = new PhpStats_TimeInterval_Day( $this->getTimeParts(), array(), false, false );
-//        $month->compact();
-    }
-
-	protected function getMonth()
+    protected function getMonth()
 	{
 		return new PhpStats_TimeInterval_Month( $this->getTimeParts() );
 	}
